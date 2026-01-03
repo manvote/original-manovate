fb18af687dc861803ac009054550aa3221351bb5
manovatetechnologies-lab <manovatetechnologies@gmail.com>
2026-01-03 16:44:44 +0530

    backend_check

--- src/components/free-consultation.js
+++ src/components/free-consultation.js
@@ -34,14 +34,9 @@ export default function FreeConsultation() {
     setStatus("sending");
 
     const finalService = service === "Other" ? otherService : service;
-
-<<<<<<< HEAD
-    try {
-      const res = await fetch("http://127.0.0.1:8000/contact-service/", {
-=======
     try { 
       const res = await fetch("https://manovatebackend.onrender.com/contact-service/", {
->>>>>>> a6bc8d8 (backend_check)
+
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
--- src/publicApi.js
+++ src/publicApi.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 const PublicAPI = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://127.0.0.1:8000/",
-=======
-  baseURL: "https://manovatebackend.onrender.com/",
->>>>>>> a6bc8d8 (backend_check)
+baseURL: "https://manovatebackend.onrender.com/",
+
 });
 
 export default PublicAPI;
